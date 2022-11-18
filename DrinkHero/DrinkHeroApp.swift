cfb9d55916109b2bb4c96d9d21ede06e482a22d1
Veer Singh <veersingh209@icloud.com>
2022-11-18 14:08:12 -0800

    Initial Commit

---
+++ DrinkHero/DrinkHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkHeroApp.swift
+//  DrinkHero
+//
+//  Created by Veer Singh on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct DrinkHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
